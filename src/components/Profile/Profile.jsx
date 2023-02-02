0436dfa67ebb6ef3f49c8165cbfc6a6701582a80
rizorno <114782234+rizorno@users.noreply.github.com>
2023-02-02 10:26:38 +0100

    updates

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -17,19 +17,36 @@ function Profile({ username, tag, location, avatar, stats }) {
       <ul className={css.stats}>
         <li>
           <span className={css.label}>Followers</span>
-          <span className={css.quantity}>{followers}</span>
+          <span className={css.quantity}>{separation(followers)}</span>
         </li>
         <li>
           <span className={css.label}>Views</span>
-          <span className={css.quantity}>{views}</span>
+          <span className={css.quantity}>{separation(views)}</span>
         </li>
         <li>
           <span className={css.label}>Likes</span>
-          <span className={css.quantity}>{likes}</span>
+          <span className={css.quantity}>{separation(likes)}</span>
         </li>
       </ul>
     </div>
   );
+}
+
+//? Separation of thousandths
+
+function separation(data) {
+  data = Math.round(data);
+  let xP = String(data);
+  let sP = xP.split('');
+  let indexP = xP.length - 1;
+  let wP = Math.floor(indexP / 3);
+  for (let i = 1; i <= wP; i += 1) {
+    indexP = indexP - 1 - i;
+    sP.splice(indexP, 0, ' ');
+    let rP = sP.join('');
+    data = rP;
+  }
+  return data;
 }
 
 Profile.propTypes = {
