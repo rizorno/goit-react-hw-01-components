ba68b0c21cad481a68c4cd85d0e44bb6706d177d
rizorno <114782234+rizorno@users.noreply.github.com>
2023-02-02 12:29:22 +0100

    updates

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -6,11 +6,29 @@ function TransactionHistory({ items = [] }) {
   let elements = items.map(element => {
     const { id, type, amount, currency } = element;
 
+    //? Separation
+
+    function separation(data) {
+      let sP = data.split('');
+      let qP = sP.filter(element => element !== ' ');
+      let sS = qP.join('');
+      let qS = String(sS);
+      let indexP = qS.length - 1;
+      let wP = Math.floor(indexP / 3);
+      for (let i = 1; i <= wP; i += 1) {
+        indexP = indexP - 1 - i;
+        qP.splice(indexP, 0, ', ');
+        let rP = qP.join('');
+        data = rP;
+      }
+      return data;
+    }
+
     return (
       <tr key={id}>
         <td>{type}</td>
         <td>{amount}</td>
-        <td>{currency}</td>
+        <td>{separation(currency)}</td>
       </tr>
     );
   });
